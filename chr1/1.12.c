a033d014b71997dc4edc136e596b715caf7aeddd
Gabriel Marin <gbrlmarn@gmail.com>
2022-10-09 01:34:11 +0300

    added make file to chapter 1

--- chr1/1.12.c
+++ chr1/1.12.c
@@ -3,24 +3,20 @@
 #define OUT 0
 #define IN 1
 
-main() 
+int main() 
 {
   int c, nl, nc, nt, state;
   state = OUT;
   nl = nc = nt = 0;
-  while((c = getchar()) != EOF)
-  {
-    if (c != ' ' && c != '\t' && c != '\n')
-      {
+  while((c = getchar()) != EOF) {
+    if (c != ' ' && c != '\t' && c != '\n') {
       putchar(c);
       state = IN;
-      }
-      else if (state)
-      {
-        putchar('\n');
-        state = OUT;
-      }
     }
-
-    return 0;
- }
+    else if (state) {
+      putchar('\n');
+      state = OUT;
+    }
+  }
+  return 0;
+}
